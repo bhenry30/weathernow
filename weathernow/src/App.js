a09acf7227ae0ccfa1af4e15e5af4cc22ff31c64
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2023-03-19 16:16:06 -0500

    adds TimeLoc component

--- weathernow/src/App.js
+++ weathernow/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import Search from './components/Search'
+import TimeLoc from './components/TimeLoc'
 
 function App() {
   return (
-    <div className="mx-auto h-screen bg-gradient-to-br from-green-900 to-emerald-600">
+    <div className="mx-auto h-screen bg-gradient-to-br from-sky-700 to-yellow-200">
     
         <Search />
-     
+        <TimeLoc />
     </div>
   );
 }
---
+++ weathernow/src/components/TimeLoc.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { UilLocationPoint, UilSearch } from "@iconscout/react-unicons"
+
+function Search() {
+  return (
+    <div>
+        <div className=' flex items-center justify-center'>
+            <p className='text-white font-extralight text-lg'>Sunday, March 19 2023</p>   
+        </div>
+        <div className='flex items-center justify-center'>
+            <p className='text-white text-3xl font-medium'>Dallas</p>
+        </div>
+    </div>    
+  )
+}
+
+export default Search
